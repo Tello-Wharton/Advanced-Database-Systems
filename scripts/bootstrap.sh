a1bed742f4b38b03a01c1054b1e3c13f86a11f8f
Aaron <aaron.tkd@hotmail.com>
2018-02-01 14:55:20 +0000

    setup posgres for lab

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,3 +4,20 @@ apt-get -y update
 apt-get -y install postgresql-client
 apt-get -y install postgresql
 apt-get -y install pgadmin3 
+
+cat <<-EOF | sudo -u postgres psql 
+	CREATE USER root WITH PASSWORD 'cake';
+	ALTER ROLE root WITH SUPERUSER;
+	
+	CREATE DATABASE adb;
+	GRANT ALL PRIVILEGES ON DATABASE adb TO root;
+EOF
+
+
+cat <<-EOF | psql -U root -d adb
+	CREATE TABLE account(id BIGSERIAL NOT NULL, name VARCHAR(30),
+		birthdat DATE, last_login TIMESTAMP, balance DOUBLE PRECISION, credits
+		INT);
+	COPY account FROM '/vagrant/resources/account.csv' WITH CSV HEADER;
+	SELECT * FROM account;
+EOF
